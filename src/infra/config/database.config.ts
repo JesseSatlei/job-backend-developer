933f908c3aaea3dac067fefb6f2575d1bd6acb98
Jesse Satlei <jessesatlei@hotmail.com>
2024-12-02 14:48:22 -0300

    feat: add possibility for news databases

--- src/infra/config/database.config.ts
+++ src/infra/config/database.config.ts
@@ -5,13 +5,15 @@ import { config } from 'dotenv';
 config();
 
 export const databaseConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: process.env.MYSQLDB_HOST || 'mysql-db',
-  port: parseInt(process.env.MYSQLDB_PORT, 10) || 3306,
-  username: process.env.MYSQLDB_USER || 'root',
-  password: process.env.MYSQLDB_PASSWORD || 'senha_root_123',
-  database: process.env.MYSQLDB_DATABASE || 'banking',
+  type: process.env.DB_TYPE as any,
+  host: process.env.MYSQLDB_HOST,
+  port: parseInt(process.env.MYSQLDB_PORT, 10),
+  username: process.env.MYSQLDB_USER,
+  password: process.env.MYSQLDB_PASSWORD,
+  database: process.env.MYSQLDB_DATABASE,
   entities: [Movie],
   synchronize: process.env.TYPEORM_SYNC === 'true',
   migrationsRun: true,
+  ssl:
+    process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
 };
